23e723f2237613b089d3e7aa8378fc18529a1bfa
yannick <yannickmacken@gmail.com>
2023-06-02 18:24:21 +0200

    Update Location.tsx

--- src/api/Location.tsx
+++ src/api/Location.tsx
@@ -5,14 +5,12 @@ async function getAddressFromCoordinates(
   longitude: number,
   setLocationName: Function
 ) {
-  console.log("Getting address from coordinates");
   const response = await supabase.functions.invoke("get-address-from-coords", {
     headers: {
       latitude: latitude.toString(),
       longitude: longitude.toString(),
     },
   });
-  console.log("Response from get-address-from-coords", response);
   setLocationName(response.data.address);
 }
 
