6405b050c6b587cd6e3644c9ecd94c1b67362e90
yannick <yannickmacken@gmail.com>
2023-06-02 18:23:33 +0200

    save language selection async

--- i18n.config.ts
+++ i18n.config.ts
@@ -1,8 +1,8 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import englishTranslation from './translations/english.json';
-import dutchTranslation from './translations/dutch.json';
-
+import englishTranslation from "./translations/english.json";
+import dutchTranslation from "./translations/dutch.json";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 //empty for now
 const resources = {
@@ -14,14 +14,18 @@ const resources = {
   },
 };
 
-i18n.use(initReactI18next).init({
-  resources,
-  lng: "english",
-  //language to use if translations in user language are not available
-  fallbackLng: "english",
-  interpolation: {
-    escapeValue: false, // not needed for react!!
-  },
-});
+const getDefaultLang = async () => {
+  // @ts-ignore
+  const storedLang: Language = await AsyncStorage.getItem("language");
+  return i18n.use(initReactI18next).init({
+    resources,
+    lng: storedLang ? storedLang : "english",
+    //language to use if translations in user language are not available
+    fallbackLng: "english",
+    interpolation: {
+      escapeValue: false, // not needed for react!!
+    },
+  });
+};
 
-export default i18n;
+export default getDefaultLang();
--- src/components/LanguageSelector.tsx
+++ src/components/LanguageSelector.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
-import { View, Image } from 'react-native';
+import { View } from 'react-native';
 import RNPickerSelect from 'react-native-picker-select';
 import { useTranslation } from 'react-i18next';
 import { LANGUAGES } from '../util/Constants';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const LanguageSelector: React.FC = () => {
   const { i18n } = useTranslation();
 
-  const changeLanguage = (language: Language) => {
+  async function changeLanguage (language: Language) {
     i18n.changeLanguage(language);
+    await AsyncStorage.setItem('language', language);
   };
 
   return (
     <View>
       <RNPickerSelect
+        value={i18n.language} // current language from i18n
         onValueChange={(value) => changeLanguage(value)}
         items={LANGUAGES.map(({ value, flag }) => ({
           label: `${flag} ${value}`,
