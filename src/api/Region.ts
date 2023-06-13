e4ce1f1c1b18ee09038ff1f7e52194e9d9419e6d
yannick <yannickmacken@gmail.com>
2023-06-13 21:49:42 +0200

    Update Region.ts

--- src/api/Region.ts
+++ src/api/Region.ts
@@ -9,15 +9,13 @@ async function getRegionFromLocation(locaction: location, language: Language, se
     response &&
     response.data &&
     // @ts-ignore
-    response.data.eco_code &&
-    // @ts-ignore
-    response.data.ecoregion_name
+    response.data.eco_code
   ) {
     // @ts-ignore
     const eco_code = response.data.eco_code;
     // @ts-ignore
     const ecoregion_name = response.data.ecoregion_name;
-    setRegion({ eco_code: eco_code, name: ecoregion_name });
+    setRegion({ eco_code: eco_code, name: ecoregion_name? ecoregion_name : null });
   } else {
     setRegion(null);
   }
