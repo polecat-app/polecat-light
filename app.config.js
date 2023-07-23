e6a301533da62a319ce968f0a3765e8ea69978cb
yannick <yannickmacken@gmail.com>
2023-07-23 12:31:37 +0200

    remove app json, android map and top bar

--- app.config.js
+++ app.config.js
@@ -3,32 +3,35 @@ import 'dotenv/config';
 export default ({ config }) => ({
   ...config,
   expo: {
-    name: "Polecat",
-    slug: "Polecat",
-    version: "1.0.1",
-    orientation: "portrait",
-    icon: "./assets/icon.png",
+    name: 'Polecat',
+    slug: 'Polecat',
+    version: '1.0.1',
+    orientation: 'portrait',
+    icon: './assets/icon.png',
     splash: {
-      image: "./assets/splash.png",
-      resizeMode: "contain",
-      backgroundColor: "#20b2aa"
+      image: './assets/splash.png',
+      resizeMode: 'contain',
+      backgroundColor: '#20b2aa'
     },
     updates: {
       fallbackToCacheTimeout: 0
     },
-    assetBundlePatterns: [
-      "**/*"
-    ],
+    assetBundlePatterns: ['**/*'],
     ios: {
+      buildNumber: '4',
       supportsTablet: true,
-      bundleIdentifier: "com.wilddevs.polecat.free"
+      bundleIdentifier: 'com.wilddevs.polecat.free'
     },
     android: {
-      adaptiveIcon: {
-        foregroundImage: "./assets/adaptive-icon.png",
-        backgroundColor: "#20b2aa"
+      versionCode: 4,
+      androidStatusBar: {
+        backgroundColor: '#008080'
       },
-      package: "com.wilddevs.polecat.free.test",
+      adaptiveIcon: {
+        foregroundImage: './assets/adaptive-icon.png',
+        backgroundColor: '#20b2aa'
+      },
+      package: 'com.wilddevs.polecat.free.test',
       config: {
         googleMaps: {
           apiKey: process.env.GOOGLE_MAPS_API_KEY,
@@ -36,11 +39,11 @@ export default ({ config }) => ({
       },
     },
     web: {
-      favicon: "./assets/favicon.png"
+      favicon: './assets/favicon.png'
     },
     extra: {
       eas: {
-        projectId: "1f23fab5-d23f-4733-976c-830f6cf3e4dc"
+        projectId: '1f23fab5-d23f-4733-976c-830f6cf3e4dc'
       }
     }
   }
