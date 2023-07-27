2b7a519ce360695f0c76abc7b40b8852d182337b
yannick <yannickmacken@gmail.com>
2023-07-27 15:36:42 +0200

    permission descriptions iOS

--- app.config.js
+++ app.config.js
@@ -1,37 +1,47 @@
-import 'dotenv/config';
+import "dotenv/config";
 
 export default ({ config }) => ({
   ...config,
   expo: {
-    name: 'Polecat',
-    slug: 'Polecat',
-    version: '1.0.1',
-    orientation: 'portrait',
-    icon: './assets/icon.png',
+    name: "Polecat",
+    slug: "Polecat",
+    version: "1.0.1",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
     splash: {
-      image: './assets/splash.png',
-      resizeMode: 'contain',
-      backgroundColor: '#20b2aa'
+      image: "./assets/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#20b2aa",
     },
     updates: {
-      fallbackToCacheTimeout: 0
+      fallbackToCacheTimeout: 0,
     },
-    assetBundlePatterns: ['**/*'],
+    assetBundlePatterns: ["**/*"],
     ios: {
-      buildNumber: '4',
+      buildNumber: "5",
       supportsTablet: true,
-      bundleIdentifier: 'com.wilddevs.polecat.free'
+      bundleIdentifier: "com.wilddevs.polecat.free",
+      infoPlist: {
+        NSLocationAlwaysAndWhenInUseUsageDescription:
+          "Turning on location services allows Polecat to find animals in your region.",
+        NSLocationWhenInUseUsageDescription:
+          "Turning on location services allows Polecat to find animals in your region.",
+        NSLocationAlwaysUsageDescription:
+          "Turning on location services allows Polecat to find animals in your region.",
+        NSLocationUsageDescription:
+          "Turning on location services allows Polecat to find animals in your region.",
+      },
     },
     android: {
-      versionCode: 4,
+      versionCode: 5,
       androidStatusBar: {
-        backgroundColor: '#008080'
+        backgroundColor: "#008080",
       },
       adaptiveIcon: {
-        foregroundImage: './assets/adaptive-icon.png',
-        backgroundColor: '#20b2aa'
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#20b2aa",
       },
-      package: 'com.wilddevs.polecat.free.test',
+      package: "com.wilddevs.polecat.free.test",
       config: {
         googleMaps: {
           apiKey: process.env.GOOGLE_MAPS_API_KEY,
@@ -39,12 +49,12 @@ export default ({ config }) => ({
       },
     },
     web: {
-      favicon: './assets/favicon.png'
+      favicon: "./assets/favicon.png",
     },
     extra: {
       eas: {
-        projectId: '1f23fab5-d23f-4733-976c-830f6cf3e4dc'
-      }
-    }
-  }
+        projectId: "1f23fab5-d23f-4733-976c-830f6cf3e4dc",
+      },
+    },
+  },
 });
